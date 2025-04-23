84eb7942aa14db6ee5e68758a2020df6a4f9f285
m-faisal930 <faisal.mr333@gmail.com>
2025-04-23 17:01:11 +0500

    jobs page removed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,8 +8,8 @@ import JobManagement from "./pages/Admin/JobManagement";
 import SettingsPage from "./pages/Admin/SettingsPage";
 import ApplicantForm from "./pages/ApplicantForm";
 import ContactPage from "./pages/ContactPage";
-import JobDetailsPage from "./pages/jobDetailsPage";
-import JobsPage from "./pages/JobsPage";
+// import JobDetailsPage from "./pages/jobDetailsPage";
+// import JobsPage from "./pages/JobsPage";
 import LandingPage from "./pages/LandingPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
@@ -23,10 +23,10 @@ function App() {
           <Routes>
             <Route path="/" element={<LandingPage />} />
             <Route path="/contact" element={<ContactPage />} />
-            <Route path="/jobs" element={<JobsPage />} />
+            {/* <Route path="/jobs" element={<JobsPage />} /> */}
             <Route path="/about" element={<AboutUsPage />} />
             <Route path="/apply/:id" element={<ApplicantForm />} />
-              <Route path="/jobs/:id" element={<JobDetailsPage />} />
+              {/* <Route path="/jobs/:id" element={<JobDetailsPage />} /> */}
             <Route path="/admin" element={<AdminLayout />}>
               <Route path="dashboard" element={<AdminDashboard />} />
               <Route path="jobs" element={<JobManagement />} />
