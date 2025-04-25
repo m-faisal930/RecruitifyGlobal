3cc957003a460dee8923cc163a580f4590865052
m-faisal930 <faisal.mr333@gmail.com>
2025-04-25 12:18:29 +0500

    bcrypjs added

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 // import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
